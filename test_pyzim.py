6047923b344909ea5973481a4030d2248d232c82
Ralf Schmitt <ralf@systemexit.de>
2011-11-03 22:48:55 +0100

    make test compatible with py.test

--- test_pyzim.py
+++ test_pyzim.py
@@ -1,3 +1,5 @@
+#! /usr/bin/env py.test
+
 import pyzim
 
 lorem = u'''<div><a href="A/Redirect">Link!!!11</a> <img src="/I/Test.jpg" width="200" height="100" /> Lorem ipsum dolor sit <strong>amet</strong>, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</div>\n'''
@@ -25,6 +27,6 @@ class TestArticleSource(pyzim.IterArticleSource):
         return u'<h1>{0}</h1>\n{1}'.format(aid, lorem)
 
 
-
-src = TestArticleSource(100)
-src.create('test.zim')
+def test_create():
+    src = TestArticleSource(100)
+    src.create('test.zim')
